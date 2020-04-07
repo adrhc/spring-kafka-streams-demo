df0e08f9449b1344adbf30af879e54f8612871e7
adrhc <adrhc@outlook.com>
2020-04-07 19:41:13 +0300

    init

---
+++ src/test/java/ro/go/adrhc/springkafkastreams/SpringKafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.go.adrhc.springkafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
