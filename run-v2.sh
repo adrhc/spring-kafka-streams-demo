1c2d26881c3d004eaf186af5e65f242f540205eb
adrhc <adrhc@outlook.com>
2020-04-15 16:12:34 +0300

    run-v2.sh logging

--- run-v2.sh
+++ run-v2.sh
@@ -2,4 +2,5 @@
 shopt -s expand_aliases
 source ~/.bash_aliases
 
-./mvnw spring-boot:run -Dspring-boot.run.profiles=v2
+# tailf app.log | egrep "client1|Notification:|Overdue:|Limit:"
+./mvnw spring-boot:run -Dspring-boot.run.profiles=v2 -Dspring-boot.run.arguments=--logging.file.name=app.log
